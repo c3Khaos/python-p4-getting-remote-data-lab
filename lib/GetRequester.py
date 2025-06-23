24089fe63d58226247f35be77307de4938a9cdcb
christopherchege <chrisshege35@gmail.com>
2025-06-23 11:29:56 +0300

    ...

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -17,9 +17,6 @@ class GetRequester:
 if __name__ == "__main__":
     url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
     requester = GetRequester(url)
+    requester.load_json()
 
-    print("Raw Response Body:")
-    print(requester.get_response_body())
-
-    print("\nParsed JSON:")
-    print(requester.load_json())
+   
