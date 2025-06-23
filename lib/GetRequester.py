145236f2aa48f128f6cbac4faa94eb100b339014
christopherchege <chrisshege35@gmail.com>
2025-06-23 11:26:27 +0300

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        response =  requests.get(self.url)
+        return response.json()
+    
+if __name__ == "__main__":
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    requester = GetRequester(url)
+
+    print("Raw Response Body:")
+    print(requester.get_response_body())
+
+    print("\nParsed JSON:")
+    print(requester.load_json())
